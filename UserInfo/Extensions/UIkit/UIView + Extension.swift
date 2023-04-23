b620d6f2a3d71b8d3b83dc95b87418284b98c886
Bezrukovandrej <bezrukovandrej@gmail.com>
2023-04-23 11:27:39 +0300

    Информация о пользователе.

---
+++ UserInfo/Extensions/UIkit/UIView + Extension.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIView {
+    
+    func addView(_ view: UIView) {
+        self.addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false 
+    }
+}
+
+
---
+++ UserInfo/Resources/Resources.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+enum Resources {
+    
+    enum NameFields: String, CaseIterable {
+        case firstName = "Имя"
+        case secondName = "Фамилия"
+        case thirdName = "Отчество"
+        case dateBirthday = "Дата рождения"
+        case gender = "Пол"
+    }
+}
